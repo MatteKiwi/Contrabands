4d0497ff948d37d94ebc39768ad5b97a74fd9210
MatteKiwi <55237242+MatteKiwi@users.noreply.github.com>
2020-01-30 13:40:32 +0100

    update

--- Contraband/Contraband/Program.cs
+++ Contraband/Contraband/Program.cs
@@ -10,6 +10,29 @@ namespace Contraband
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hur många bilar ska skapas?");
+            int n = 0;
+            while (n <= 1)
+            {
+                if (int.TryParse(Console.ReadLine(), out n) && n > 0)
+                {
+                    continue;
+                }
+                Console.WriteLine("Icke sa micke! försök igen!!");
+            }
+
+            List<Car> cars = new List<Car>();
+            Random rng = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                if(rng.Next(2) != 0)
+                {
+
+                }
+            }
+
+
             Console.ReadLine();
         }
     }
