69e794ff7d6fe4ccce4c36eb09b33cab205d0b53
MatteKiwi <55237242+MatteKiwi@users.noreply.github.com>
2020-01-30 13:29:55 +0100

    Create ContrabandCar.cs

---
+++ Contraband/Contraband/ContrabandCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contraband
+{
+    class ContrabandCar : Car
+    {
+      public ContrabandCar()
+        {
+            passengers = gen.Next(1, 3);
+            contrabandAmount = gen.Next(1, 5);
+        }
+    }
+}
